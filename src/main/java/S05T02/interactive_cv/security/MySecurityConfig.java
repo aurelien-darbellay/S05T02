a61096b2e9c0847e1129db017682cf7c743a92fb
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-05-21 15:10:12 +0200

    back

--- src/main/java/S05T02/interactive_cv/security/MySecurityConfig.java
+++ src/main/java/S05T02/interactive_cv/security/MySecurityConfig.java
@@ -6,23 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.ServerCsrfTokenRepository;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -36,15 +28,15 @@ import java.util.List;
 public class MySecurityConfig {
 
     @Bean
-    SecurityWebFilterChain filterChain(ServerHttpSecurity http, /*CorsConfigurationSource corsConfig,*/ ServerCsrfTokenRepository csrfTokenRepository, ServerAuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
+    SecurityWebFilterChain filterChain(ServerHttpSecurity http, CorsConfigurationSource corsConfig, ServerCsrfTokenRepository csrfTokenRepository, ServerAuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
         http
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-                //.cors(cors -> cors.configurationSource(corsConfig))
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                /*.csrf(csrf ->
+                .cors(cors -> cors.configurationSource(corsConfig))
+                //.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .csrf(csrf ->
                         csrf.csrfTokenRepository(csrfTokenRepository)
                                 .csrfTokenRequestHandler(new ServerCsrfTokenRequestAttributeHandler())
-                )*/
+                )
                 .authorizeExchange(exchanges ->
                         exchanges
                                 .pathMatchers(HttpMethod.OPTIONS).permitAll()
@@ -60,7 +52,7 @@ public class MySecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080"));  // your front-end origin
+        cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));  // your front-end origin
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-XSRF-TOKEN"));
         cfg.setAllowCredentials(false);
@@ -85,15 +77,16 @@ public class MySecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
-    ServerAuthenticationSuccessHandler authenticationSuccessHandler(){
+    ServerAuthenticationSuccessHandler authenticationSuccessHandler() {
         return (webFilterExchange, authentication) ->
                 Mono.defer(() -> {
-            ServerHttpResponse response = webFilterExchange.getExchange().getResponse();
-            response.setStatusCode(HttpStatus.SEE_OTHER);
-            response.getHeaders().setLocation(URI.create("/csrf"));
-            return response.setComplete();
-        });
+                    ServerHttpResponse response = webFilterExchange.getExchange().getResponse();
+                    response.setStatusCode(HttpStatus.SEE_OTHER);
+                    response.getHeaders().setLocation(URI.create("/csrf"));
+                    return response.setComplete();
+                });
     }
 }
 
